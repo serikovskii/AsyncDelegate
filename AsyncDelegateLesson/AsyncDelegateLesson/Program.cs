33db45725f2a0e34c65e12084b1b552b2a04464e
Azat <46015824+serikovskii@users.noreply.github.com>
2019-06-06 12:21:27 +0600

    theory

---
+++ AsyncDelegateLesson/AsyncDelegateLesson/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDelegateLesson
+{
+    class Program
+    {
+        private delegate int AsyncDelegate(int first, int second);
+        static void Main(string[] args)
+        {
+            GetCurrentThreadInfo();
+            var executer = new AsyncDelegate(Sum);
+            var asyncResult = executer.BeginInvoke(10, 15, SumCallback, null);
+
+            //var result = executer.EndInvoke(asyncResult);
+            //Sum(10, 15);
+
+            Console.ReadLine();
+        }
+
+        public static void SumCallback(IAsyncResult result)
+        {
+            var stringData = result.AsyncState.ToString();
+            var executer = (result as AsyncResult).AsyncDelegate as AsyncDelegate;
+            var dataResult = executer.EndInvoke(result);
+            Console.WriteLine(stringData + dataResult);
+        }
+        public static int Sum(int firstNumber, int secondNumber)
+        {
+            GetCurrentThreadInfo();
+
+            Thread.Sleep(5000);
+            return firstNumber + secondNumber;
+            //Console.WriteLine($"{firstNumber.ToString()} + {secondNumber.ToString()} = {firstNumber+secondNumber}");
+        }
+
+        public static void GetCurrentThreadInfo()
+        {
+            var currentThread = Thread.CurrentThread;
+            Console.WriteLine($"Происходит работа в потоке {currentThread.ManagedThreadId}");
+        }
+    }
+}
